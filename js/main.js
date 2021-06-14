ea7bc8fb972a0e63ac68453b7fb97980b1a3dc9d
davidtheblane <davidtheblane@gmail.com>
2021-06-14 12:46:23 -0300

    basic model finalized

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ function cadastraVeiculo(e) {
   const placaCarro = document.getElementById("placaCarro").value;
   const time = new Date();
 
+  if (!modeloCarro && !placaCarro) {
+    alert("Por Favor preencha todos os campos.");
+    return false;
+  }
+
   carro = {
     modelo: modeloCarro,
     placa: placaCarro,
@@ -23,6 +28,42 @@ function cadastraVeiculo(e) {
     carros.push(carro);
     localStorage.setItem("patio2", JSON.stringify(carros));
   }
+  document.getElementById("formulario").reset();
+
+  mostraPatio();
 
   e.preventDefault();
+}
+
+function apagarVeiculo(placa) {
+  var carros = JSON.parse(localStorage.getItem("patio2"));
+
+  for (let i = 0; i < carros.length; i++) {
+    if (carros[i].placa == placa) {
+      carros.splice(i, 1);
+    }
+    localStorage.setItem("patio2", JSON.stringify(carros));
+  }
+  mostraPatio();
+}
+
+function mostraPatio() {
+  var carros = JSON.parse(localStorage.getItem("patio2"));
+  var carrosResultado = document.getElementById("resultados");
+
+  carrosResultado.innerHTML = "";
+
+  for (let i = 0; i < carros.length; i++) {
+    let modelo = carros[i].modelo;
+    let placa = carros[i].placa;
+    let hora = carros[i].hora;
+    let minutos = carros[i].minutos;
+
+    carrosResultado.innerHTML += `<tr>
+    <td> ${modelo} </td>
+    <td> ${placa} </td>
+    <td> ${hora}: ${minutos} </td>
+    <td><button class="btn btn-danger" onclick="apagarVeiculo('${placa}')"> Excluir </button></td>
+    </tr>`;
+  }
 }
